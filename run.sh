95f14878c2c6ff8ed2247a0e79a05cc3934e5413
shaun <dukenan006@163.com>
2022-11-21 11:14:37 +0800

    fix(branch): delete branch

--- run.sh
+++ run.sh
@@ -35,6 +35,7 @@ function commitRepository () {
    git config --global user.email "dukenan006@163.com"
    git config --global user.name "shaun"
    git commit -m "update README,  $(date "+%a %b %d %H:%M %Z")" || echo "No changes to commit"
+   git checkout -D shaun
    git push -f
 }
 
